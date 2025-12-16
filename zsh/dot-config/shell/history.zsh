e5f86d2b26ccf95dfcca5e92b002ec8bacb33cef
Vantasin <76798329+Vantasin@users.noreply.github.com>
2025-12-16 09:12:22 -0500

    Update

---
+++ zsh/dot-config/shell/history.zsh
@@ -0,0 +1,7 @@
+# History behavior tuned for autosuggestions and shared history.
+setopt append_history
+setopt share_history
+setopt inc_append_history
+setopt hist_ignore_dups
+setopt hist_ignore_all_dups
+setopt hist_reduce_blanks
