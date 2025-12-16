0b23eef43a56bc4095684ccc2a1864188730b893
Vantasin <76798329+Vantasin@users.noreply.github.com>
2025-12-16 10:38:53 -0500

    Update

--- zsh/dot-config/shell/prompt.zsh
+++ zsh/dot-config/shell/prompt.zsh
@@ -1,10 +1,14 @@
 # Minimal prompt with git branch awareness.
 
+setopt prompt_subst
+
 autoload -Uz add-zsh-hook colors vcs_info
 colors
 
-zstyle ':vcs_info:git:*' formats '%F{yellow}(%b)%f'
-zstyle ':vcs_info:git:*' actionformats '%F{yellow}(%b|%a)%f'
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' formats '%F{yellow}(%b%u%f%F{red}%m%f)%f'
+zstyle ':vcs_info:git:*' actionformats '%F{yellow}(%b|%a%u%f%F{red}%m%f)%f'
 
 _prompt_vcs_info() { vcs_info }
 add-zsh-hook precmd _prompt_vcs_info
