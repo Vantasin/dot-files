d236b2bcb5e8e9cefccfbe88356a21b22bbe122c
Vantasin <76798329+Vantasin@users.noreply.github.com>
2025-12-16 10:23:01 -0500

    Update

--- zsh/dot-config/shell/prompt.zsh
+++ zsh/dot-config/shell/prompt.zsh
@@ -6,10 +6,10 @@ colors
 zstyle ':vcs_info:git:*' formats '%F{yellow}(%b)%f'
 zstyle ':vcs_info:git:*' actionformats '%F{yellow}(%b|%a)%f'
 
-_prompt_vcs_info() {
-  vcs_info
-}
+_prompt_vcs_info() { vcs_info }
 add-zsh-hook precmd _prompt_vcs_info
 
-PROMPT=$'%F{cyan}%T%f %F{white}%n%f%F{magenta}@%f%F{white}%m%f %F{green}%~%f ${vcs_info_msg_0_}\n%F{yellow}%#%f '
-RPROMPT=''
+# Use prompt_subst so ${vcs_info_msg_0_} expands.
+PROMPT="%F{cyan}%T%f %F{white}%n%f%F{magenta}@%f%F{white}%m%f %F{green}%~%f ${vcs_info_msg_0_}
+%F{yellow}%#%f "
+RPROMPT=""
